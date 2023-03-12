a65c8b961ba8fa3eac32088381b99ad5d2141621
Cryptomnesia <82593272+CryptomnesiaWasTaken@users.noreply.github.com>
2023-03-12 02:08:46 +0100

    Update URI.js

--- URI.js
+++ URI.js
@@ -1,7 +1,7 @@
 const isDevelopmentMode = process.env.MODE === "DEV";
 
 const clientURL = isDevelopmentMode
-	? "http://localhost:3000"
-	: ["https://adra-amie.netlify.app", "https://fb-clone-frontend.vercel.app"];
+	? "https://scoutx.onrender.com"
+	: ["https://scoutx.onrender.com", "https://scoutx.onrender.com"];
 
 module.exports = { clientURL };
